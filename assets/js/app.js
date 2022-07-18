c47f51173e632947efc4c5b1b967d20c7992e006
Robina <92825274+DawoodRobina@users.noreply.github.com>
2022-07-18 12:37:47 +0530

    Travel Agency files

---
+++ assets/js/app.js
@@ -0,0 +1,35 @@
+$('#destinations-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+
+/***Review****/
+
+$('#review-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:2
+        }
+    }
+})
